502f4544292930f33ade56da02e0103e676cee20
SreeCharan <sreecharan309@gmail.com>
2025-02-22 23:19:17 +0530

    Added : vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
-
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello from backend');
 });
 
-
-app.get('/health-check', (res) => {
-    res.send('Health ckeck OK');
+app.get('/health-check', (req, res) => {
+    res.send('Health check OK');
 });
-
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
